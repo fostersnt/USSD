0cd658f3290abb4e3a99ab6b77eecfddd1dd15d8
= <=>
2023-11-26 09:31:55 +0000

    Added pag menus in account_check.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 include('./functions.php');
+include('./secrets.php');
 
+$app_secrets = getSecrets();
 
 echo printName("Foster Amponsah Asante");
+
+echo "<br>";
+echo "<br>";
+
+echo $app_secrets["apiKey"];
---
+++ menu/account_check.php
@@ -0,0 +1,6 @@
+<?php
+
+function page_1()
+{
+    return "Dear customer,\nYou are welcome to the self registration service.\n1. Check Account Balance\n2.Close account";
+}
