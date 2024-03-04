7550e4b9b98450b92e6b9731f281a4d748afa802
= <=>
2024-03-04 12:35:40 +0000

    Added a class to return the various screen

---
+++ classes/Response.php
@@ -0,0 +1,11 @@
+<?php
+
+class Response
+{
+    public static function screenOne()
+    {
+        return "Hello, you are welcome to this USSD screen.Select an option to proceed\n
+        1. Request Transaction Details\n
+        2. View Personal info";
+    }
+}
--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <?php
-include('./functions.php');
-include('./secrets.php');
+// include('./functions.php');
+// include('./secrets.php');
 include('./menu/account_check.php');
 include('./menu/account_register.php');
 include('./api_calls/rapid_api.php');
 
-$app_secrets = getSecrets();
-$accounts_page_1 = page_1();
+try {
+    //GETTING THE REQUIRED DATA
+    $msisdn = $_GET['msisdn'] ?? null;
+    $currentPage = $_GET['currentPage'] ?? null;
+    $userInput = $_GET['userInput'] ?? null;
+    $session_id = $_GET['session_id'] ?? null;
 
-$code = "419";
+    // $app_secrets = getSecrets();
+    $accounts_page_1 = page_1();
 
-// switch ($code) {
-//     case '123':
-//         echo printName("Foster Amponsah Asante");
-//         break;
-//     case '419':
-//         echo $app_secrets["rapidAPI"]['X-RapidAPI-Key'];
-//         break;
-//     case '41':
-//         echo $accounts_page_1;
-//         break;
-//     default:
-//         echo "Your input couldn't match any item";
-//         break;
-// }
+    $code = "419";
 
+    switch ($code) {
+        case '123':
+            echo printName("Foster Amponsah Asante");
+            break;
+        case '419':
+            // echo $app_secrets["rapidAPI"]['X-RapidAPI-Key'];
+            echo $msisdn;
+            break;
+        case '41':
+            echo $accounts_page_1;
+            break;
+        default:
+            echo "Your input couldn't match any item";
+            break;
+    }
+} catch (\Throwable $th) {
+    echo '<span style="color: red;">' . $th->getMessage() . '</span>';
+}
